7e96388365685388b48bb7592a6894f1e93e5c02
emmanuelTwumasi <protwumasi@gmail.com>
2025-01-30 14:15:40 +0000

    add docker docs and changes to cors

--- hng-1/src/main/java/com/emma/hng/CorsConfig.java
+++ hng-1/src/main/java/com/emma/hng/CorsConfig.java
@@ -6,16 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("*") // or use "*" to allow all origins
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // allow necessary methods
-                    .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                            "Access-Control-Request-Headers", "Authorization") // allow necessary headers
-                    .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                    .allowCredentials(true) // allow credentials if necessary
-                    .maxAge(3600);
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Allows all origins (TEMPORARY)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*") // Allows all headers
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .maxAge(3600);
     }
+}
 
